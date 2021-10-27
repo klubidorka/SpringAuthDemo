b4c770323ab2ce1050ac85b2105a7a4703dd4096
Ivan <39248996+kLubidorka@users.noreply.github.com>
2021-10-27 10:19:01 +0300

    refactor

--- src/main/java/com/github/klubidorka/springsecurity/security/SecurityConfigurer.java
+++ src/main/java/com/github/klubidorka/springsecurity/security/SecurityConfigurer.java
@@ -41,12 +41,13 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf().disable()
-                .authorizeRequests().antMatchers("/auth").permitAll().
-                anyRequest().authenticated().and().
-                exceptionHandling().and().sessionManagement()
+        httpSecurity
+                .csrf().disable()
+                .authorizeRequests().antMatchers("/auth").permitAll()
+                .anyRequest().authenticated()
+                .and().exceptionHandling()
+                .and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-
     }
 }
